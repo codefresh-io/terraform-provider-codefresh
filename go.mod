7208b4ac2844273b16ad75fed526b15cdd94a0ad
Pavel Nosovets <pavel.nosovets@codefresh.io>
2020-05-19 16:25:37 +0300

    Add codefresh/
    Add vendor/

--- go.mod
+++ go.mod
@@ -1,9 +1,8 @@
-module github.com/lightstep/terraform-provider-codefresh
+module github.com/codefresh-io/terraform-provider-codefresh
 
 require (
-	github.com/hashicorp/terraform v0.12.21
+	github.com/hashicorp/terraform v0.12.21 // indirect
 	github.com/hashicorp/terraform-plugin-sdk v1.7.0
-	github.com/stretchr/testify v1.3.0
 )
 
 go 1.13
--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"github.com/codefresh-io/terraform-provider-codefresh/codefresh"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return codefresh.Provider()
 		},
 	})
 }
