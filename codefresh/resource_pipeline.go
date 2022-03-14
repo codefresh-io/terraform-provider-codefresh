42504ed22017f689d7a4d2d935a69279aa01c054
Vadim Gusev <vadim.gusev@codefresh.io>
2022-03-14 17:00:00 +0300

    run go fmt

--- codefresh/resource_pipeline.go
+++ codefresh/resource_pipeline.go
@@ -509,11 +509,10 @@ func flattenSpec(spec cfClient.Spec) []interface{} {
 		m["options"] = resOptions
 	}
 
-  m["pack_id"] = spec.PackId
+	m["pack_id"] = spec.PackId
 	m["concurrency"] = spec.Concurrency
 	m["branch_concurrency"] = spec.BranchConcurrency
 	m["trigger_concurrency"] = spec.TriggerConcurrency
-
 
 	m["priority"] = spec.Priority
 
@@ -639,7 +638,7 @@ func mapResourceToPipeline(d *schema.ResourceData) *cfClient.Pipeline {
 			OriginalYamlString: originalYamlString,
 		},
 		Spec: cfClient.Spec{
-			PackId:							d.Get("spec.0.pack_id").(string),
+			PackId:             d.Get("spec.0.pack_id").(string),
 			Priority:           d.Get("spec.0.priority").(int),
 			Concurrency:        d.Get("spec.0.concurrency").(int),
 			BranchConcurrency:  d.Get("spec.0.branch_concurrency").(int),
