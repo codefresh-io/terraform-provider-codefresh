5ea7f488df7a5983f0c651e879fada3149cda7b6
Yonatan Koren <10080107+korenyoni@users.noreply.github.com>
2022-09-25 16:48:04 +0300

    (WIP): fix cron trigger tests.

--- codefresh/resource_pipeline_cron_trigger_test.go
+++ codefresh/resource_pipeline_cron_trigger_test.go
@@ -17,7 +17,7 @@ func TestAccCodefreshPipelineCronTrigger_basic(t *testing.T) {
 	resourceName := "codefresh_pipeline_cron_trigger.test"
 	var pipelineCronTrigger cfClient.HermesTrigger
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckCodefreshPipelineCronTriggerDestroy,
