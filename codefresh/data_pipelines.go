fd31da2213b97f6427a9e471d06731182a57b239
Yonatan Koren <10080107+korenyoni@users.noreply.github.com>
2023-03-12 03:21:31 +0200

    Basic pipeline data source.

--- codefresh/data_pipelines.go
+++ codefresh/data_pipelines.go
@@ -2,6 +2,7 @@ package codefresh
 
 import (
 	"fmt"
+	"regexp"
 	"time"
 
 	cfClient "github.com/codefresh-io/terraform-provider-codefresh/client"
@@ -10,32 +11,18 @@ import (
 
 func dataSourcePipelines() *schema.Resource {
 	return &schema.Resource{
-		Description: "This resource retrives Pipeline based on the provided attributes. Currently `spec` is limited to the YAML spec.",
+		Description: "This resource retrives Pipeline based on the provided attributes.",
 		Read:        dataSourcePipelinesRead,
 		Schema: map[string]*schema.Schema{
-			"filter": {
-				Description: "The query to use when retrieving pipelines. See: [API Documentation](https://g.codefresh.io/api/#operation/pipelines-get-names).",
-				Type:        schema.TypeSet,
+			"name_regex": {
+				Description: "The name regular expression to filter pipelines by.",
+				Type:        schema.TypeString,
 				Optional:    true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"name": {
-							Description: "The name of the attribute to filter by. Must be a valid path to a field in the pipeline object. Accepts regex expressions.",
-							Type:        schema.TypeString,
-							Required:    true,
-						},
-						"values": {
-							Description: "The acceptable values of the filter.",
-							Type:        schema.TypeList,
-							Required:    true,
-							Elem:        schema.TypeString,
-						},
-					},
-				},
 			},
 			"pipelines": {
-				Type:     schema.TypeList,
-				Computed: true,
+				Description: "The returned list of pipelines. Note that `spec` is currently limited to the YAML, because of the complexity of the object.",
+				Type:        schema.TypeList,
+				Computed:    true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"id": {
@@ -104,37 +91,19 @@ func mapDataPipelinesToResource(pipelines []cfClient.Pipeline, d *schema.Resourc
 	}
 
 	filteredPipelines := make([]map[string]interface{}, 0)
-	for i, p := range res {
+	for _, p := range res {
 		match := false
-		if d.Get(fmt.Sprintf("filter.%d", i)).(*schema.Set).Len() == 0 {
+
+		name, ok := d.GetOk("name_regex")
+		if !ok {
 			match = true
+		} else {
+			r, err := regexp.Compile(name.(string))
+			if err != nil {
+				return fmt.Errorf("`name_regex` is not a valid regular expression, %s", err.Error())
+			}
+			match = r.MatchString(p["name"].(string))
 		}
-
-		//for _, f := range d.Get("filter").(*schema.Set).List() {
-		//	name := f.(map[string]interface{})["name"].(string)
-		//	values := f.(map[string]interface{})["values"].([]string)
-
-		//	attribute, ok := p[name]
-		//	if !ok {
-		//		return errors.New("Invalid filter name: " + name)
-		//	}
-
-		//	for _, v := range values {
-		//		r, err := regexp.Compile(v)
-		//		if err != nil {
-		//			return errors.New("Not a valid regular expression: " + v)
-		//		}
-
-		//		switch attribute.(type) {
-		//		case bool, string:
-		//			match = match || r.MatchString(attribute.(string))
-		//		case []string:
-		//			for _, s := range attribute.([]string) {
-		//				match = match || r.MatchString(s)
-		//			}
-		//		}
-		//	}
-		//}
 
 		if match {
 			filteredPipelines = append(filteredPipelines, p)
