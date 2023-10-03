b4e33306945b0d262abfc1548341378660f4b8f0
Yonatan Koren <10080107+korenyoni@users.noreply.github.com>
2023-10-03 20:26:51 +0300

    Clean up examples.

--- examples/account_user_associations/main.tf
+++ examples/account_user_associations/main.tf
@@ -1,8 +1,8 @@
 resource "codefresh_account_user_association" "user" {
-    email = "terraform-test-user+user-change@codefresh.io"
+    email = "terraform-test-user+user@codefresh.io"
 }
 
 resource "codefresh_account_user_association" "admin" {
-    email = "terraform-test-user+admin-change@codefresh.io"
+    email = "terraform-test-user+admin@codefresh.io"
     admin = true
 }
