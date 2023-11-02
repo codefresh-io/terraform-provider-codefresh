f7da9cd6d76ccbcd34963564d733d4cf2b36782e
Yonatan Koren <10080107+korenyoni@users.noreply.github.com>
2023-11-02 12:34:20 -0400

    Fix scripts.

--- testing/equivalence/compare-results.sh
+++ testing/equivalence/compare-results.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+path=$(which realpath)
 if [[ "$OSTYPE" == "darwin"* ]];
 then
     if ! command -v grealpath &> /dev/null;
@@ -7,13 +8,13 @@ then
         print_style "ERROR: This script requires grealpath when run on Mac. Please install it with 'brew install coreutils'\n" "danger"
         exit 1
     else
-        alias realpath=grealpath
+        path=$(which grealpath)
     fi
 fi
 
-source $(grealpath $(dirname $0))/lib.sh
+source $($path $(dirname $0))/lib.sh
 
-for i in $(find $(grealpath $(dirname $0))/results/terraform -mindepth 1 -maxdepth 1 -type d); do
+for i in $(find $($path $(dirname $0))/results/terraform -mindepth 1 -maxdepth 1 -type d); do
     test_case_name=$(basename ${i})
     print_style "Test case "
     print_style "${test_case_name}" "info"
@@ -25,8 +26,8 @@ for i in $(find $(grealpath $(dirname $0))/results/terraform -mindepth 1 -maxdep
         print_style "results/opentofu/$(basename ${f})" "info"
         print_style ": "
 
-        terraform_file=$(grealpath $(dirname $0))/results/terraform/$(basename ${i})/$(basename ${f})
-        opentofu_file=$(grealpath $(dirname $0))/results/opentofu/$(basename ${i})/$(basename ${f})
+        terraform_file=$($path $(dirname $0))/results/terraform/$(basename ${i})/$(basename ${f})
+        opentofu_file=$($path $(dirname $0))/results/opentofu/$(basename ${i})/$(basename ${f})
 
         # We normalize the JSON output to make them comparable regardless of key order
         diff \
