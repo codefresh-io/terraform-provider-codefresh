85b3d75fa2fde4e8f6b60f186df913bf4055b2fb
Yonatan Koren <10080107+korenyoni@users.noreply.github.com>
2024-01-20 23:03:34 -0500

    Remove name attribute.

--- codefresh/resource_pipeline_test.go
+++ codefresh/resource_pipeline_test.go
@@ -446,7 +446,6 @@ func TestAccCodefreshPipeline_CronTriggers(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckCodefreshPipelineExists(resourceName, &pipeline),
 					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.#", "0"),
-					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.0.name", "cT1"),
 					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.0.message", "first"),
 					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.0.expression", "0/1 * 1/1 * *"),
 					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.0.runtime_environment.0.name", "runtime1"),
@@ -456,7 +455,6 @@ func TestAccCodefreshPipeline_CronTriggers(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.0.runtime_environment.0.required_available_storage", "1gb"),
 
 					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.#", "0"),
-					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.1.name", "cT2"),
 					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.1.message", "second"),
 					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.1.expression", "0/1 * 1/1 * *"),
 					resource.TestCheckResourceAttr(resourceName, "spec.0.cron_trigger.1.git_trigger_id", "64abd1550f02a62699b10df7"),
