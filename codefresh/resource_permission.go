dd0daf913b30ed35a889e9ee99414b94a60084d5
ilia-medvedev-codefresh <ilia.medvedev@codefresh.io>
2025-05-20 15:48:18 +0300

    feat: add runtime-environment resource support to codefresh_permission resource (#164)
    ## What
    
    ## Why
    
    ## Notes
    <!-- Add any notes here -->
    
    ## Checklist
    
    * [ ] _I have read
    [CONTRIBUTING.md](https://github.com/codefresh-io/terraform-provider-codefresh/blob/master/CONTRIBUTING.md)._
    * [ ] _I have [allowed changes to my fork to be
    made](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/allowing-changes-to-a-pull-request-branch-created-from-a-fork)._
    * [ ] _I have added tests, assuming new tests are warranted_.
    * [ ] _I understand that the `/test` comment will be ignored by the CI
    trigger [unless it is made by a repo admin or
    collaborator](https://codefresh.io/docs/docs/pipelines/triggers/git-triggers/#support-for-building-pull-requests-from-forks)._

--- codefresh/resource_permission.go
+++ codefresh/resource_permission.go
@@ -10,7 +10,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	funk "github.com/thoas/go-funk"
 )
 
 func resourcePermission() *schema.Resource {
@@ -41,6 +40,7 @@ The type of resources the permission applies to. Possible values:
 	* pipeline
 	* cluster
 	* project
+	* runtime-environment
 				`,
 				Type:     schema.TypeString,
 				Required: true,
@@ -48,6 +48,7 @@ The type of resources the permission applies to. Possible values:
 					"pipeline",
 					"cluster",
 					"project",
+					"runtime-environment",
 				}, false),
 			},
 			"related_resource": {
@@ -65,7 +66,7 @@ Specifies the resource to use when evaluating the tags. Possible values:
 				Description: `
 Action to be allowed. Possible values:
 	* create
-	* read
+	* read (For runtime-environment resource, 'read' means 'assign')
 	* update
 	* delete
 	* run (Only valid for pipeline resource)
@@ -110,7 +111,7 @@ func resourcePermissionCustomDiff(ctx context.Context, diff *schema.ResourceDiff
 		}
 	}
 	if diff.HasChanges("resource", "action") {
-		if funk.Contains([]string{"run", "approve", "debug"}, diff.Get("action").(string)) && diff.Get("resource").(string) != "pipeline" {
+		if contains([]string{"run", "approve", "debug"}, diff.Get("action").(string)) && diff.Get("resource").(string) != "pipeline" {
 			return fmt.Errorf("action %v is only valid when resource is 'pipeline'", diff.Get("action").(string))
 		}
 	}
