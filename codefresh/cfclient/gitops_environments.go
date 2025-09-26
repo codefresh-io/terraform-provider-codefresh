2b2d5083f9a9bd4bf464c49c3fea0c9bdad52837
ilia-medvedev-codefresh <ilia.medvedev@codefresh.io>
2025-09-26 16:06:59 +0300

    feat: Add gitops environments resource (#172)
    ## What
    
    ## Why
    
    ## Notes
    <!-- Add any notes here -->
    
    ## Checklist
    
    * [ ] _I have read
    [CONTRIBUTING.md](https://github.com/codefresh-io/terraform-provider-codefresh/blob/master/CONTRIBUTING.md)._
    * [ ] _I have [allowed changes to my fork to be
    made](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/allowing-changes-to-a-pull-request-branch-created-from-a-fork)._
    * [ ] _I have added tests, assuming new tests are warranted_.
    * [ ] _I understand that the `/test` comment will be ignored by the CI
    trigger [unless it is made by a repo admin or
    collaborator](https://codefresh.io/docs/docs/pipelines/triggers/git-triggers/#support-for-building-pull-requests-from-forks)._

---
+++ codefresh/cfclient/gitops_environments.go
@@ -0,0 +1,206 @@
+package cfclient
+
+import (
+	"fmt"
+)
+
+const (
+	environmentQueryFields = `
+		id
+		name
+		kind
+		clusters {
+			runtimeName
+			name
+			server
+			namespaces
+		}
+		labelPairs
+	`
+)
+
+// GitopsEnvironment represents a GitOps environment configuration.
+type GitopsEnvironment struct {
+	ID         string                     `json:"id,omitempty"`
+	Name       string                     `json:"name"`
+	Kind       string                     `json:"kind"`
+	Clusters   []GitopsEnvironmentCluster `json:"clusters"`
+	LabelPairs []string                   `json:"labelPairs"`
+}
+
+// GitopsCluster represents a cluster within a GitOps environment.
+type GitopsEnvironmentCluster struct {
+	Name        string   `json:"name"`
+	RuntimeName string   `json:"runtimeName"`
+	Namespaces  []string `json:"namespaces"`
+}
+
+type GitopsEnvironmentResponse struct {
+	Errors []GraphQLError `json:"errors,omitempty"`
+	Data   struct {
+		Environment       GitopsEnvironment `json:"environment,omitempty"`
+		CreateEnvironment GitopsEnvironment `json:"createEnvironment,omitempty"`
+		UpdateEnvironment GitopsEnvironment `json:"updateEnvironment,omitempty"`
+		DeleteEnvironment GitopsEnvironment `json:"deleteEnvironment,omitempty"`
+	} `json:"data"`
+}
+
+type GitopsEnvironmentsResponse struct {
+	Data struct {
+		Environments []GitopsEnvironment `json:"environments,omitempty"`
+	} `json:"data"`
+}
+
+// At the time of writing Codefresh Graphql API does not support fetching environment by ID, So all environemnts are fetched and filtered within this client
+func (client *Client) GetGitopsEnvironments() (*[]GitopsEnvironment, error) {
+
+	request := GraphQLRequest{
+		Query: fmt.Sprintf(`
+			query ($filters: EnvironmentFilterArgs) {
+				environments(filters: $filters) {
+					%s
+				}
+			}
+		`, environmentQueryFields),
+		Variables: map[string]interface{}{},
+	}
+
+	response, err := client.SendGqlRequest(request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var gitopsEnvironmentsResponse GitopsEnvironmentsResponse
+	err = DecodeGraphQLResponseInto(response, &gitopsEnvironmentsResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &gitopsEnvironmentsResponse.Data.Environments, nil
+}
+
+func (client *Client) GetGitopsEnvironmentById(id string) (*GitopsEnvironment, error) {
+	environments, err := client.GetGitopsEnvironments()
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, env := range *environments {
+		if env.ID == id {
+			return &env, nil
+		}
+	}
+
+	return nil, nil
+}
+
+func (client *Client) CreateGitopsEnvironment(environment *GitopsEnvironment) (*GitopsEnvironment, error) {
+	request := GraphQLRequest{
+		Query: fmt.Sprintf(`
+			mutation ($environment: CreateEnvironmentArgs!) {
+				createEnvironment(environment: $environment) {
+					%s
+				}
+			}
+		`, environmentQueryFields),
+		Variables: map[string]interface{}{
+			"environment": environment,
+		},
+	}
+
+	response, err := client.SendGqlRequest(request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var gitopsEnvironmentResponse GitopsEnvironmentResponse
+	err = DecodeGraphQLResponseInto(response, &gitopsEnvironmentResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(gitopsEnvironmentResponse.Errors) > 0 {
+		return nil, fmt.Errorf("CreateGitopsEnvironment - %s", gitopsEnvironmentResponse.Errors)
+	}
+
+	return &gitopsEnvironmentResponse.Data.CreateEnvironment, nil
+}
+
+func (client *Client) DeleteGitopsEnvironment(id string) (*GitopsEnvironment, error) {
+
+	type deleteEnvironmentArgs struct {
+		ID string `json:"id"`
+	}
+
+	request := GraphQLRequest{
+		Query: fmt.Sprintf(`
+			mutation ($environment: DeleteEnvironmentArgs!) {
+  				deleteEnvironment(environment: $environment) {
+					%s
+				}
+			}
+		`, environmentQueryFields),
+
+		Variables: map[string]interface{}{
+			"environment": deleteEnvironmentArgs{
+				ID: id,
+			},
+		},
+	}
+
+	response, err := client.SendGqlRequest(request)
+
+	if err != nil {
+		return nil, err
+	}
+	var gitopsEnvironmentResponse GitopsEnvironmentResponse
+	err = DecodeGraphQLResponseInto(response, &gitopsEnvironmentResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(gitopsEnvironmentResponse.Errors) > 0 {
+		return nil, fmt.Errorf("DeleteGitopsEnvironment - %s", gitopsEnvironmentResponse.Errors)
+	}
+
+	return &gitopsEnvironmentResponse.Data.DeleteEnvironment, nil
+}
+
+func (client *Client) UpdateGitopsEnvironment(environment *GitopsEnvironment) (*GitopsEnvironment, error) {
+	request := GraphQLRequest{
+		Query: fmt.Sprintf(`
+			mutation ($environment: UpdateEnvironmentArgs!) {
+  				updateEnvironment(environment: $environment) {
+					%s
+				}
+			}
+		`, environmentQueryFields),
+		Variables: map[string]interface{}{
+			"environment": environment,
+		},
+	}
+
+	response, err := client.SendGqlRequest(request)
+
+	if err != nil {
+		return nil, err
+	}
+	var gitopsEnvironmentResponse GitopsEnvironmentResponse
+	err = DecodeGraphQLResponseInto(response, &gitopsEnvironmentResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(gitopsEnvironmentResponse.Errors) > 0 {
+		return nil, fmt.Errorf("UpdateGitopsEnvironment - %s", gitopsEnvironmentResponse.Errors)
+	}
+
+	return &gitopsEnvironmentResponse.Data.UpdateEnvironment, nil
+}
---
+++ codefresh/resource_gitops_environment_test.go
@@ -0,0 +1,127 @@
+package codefresh
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/codefresh-io/terraform-provider-codefresh/codefresh/cfclient"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccCodefreshGitopsEnvironmentsResource(t *testing.T) {
+	resourceName := "codefresh_gitops_environment.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCodefreshGitopsEnvironmentDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCodefreshGitopsEnvironmentConfig(
+					"test-for-tf",
+					"NON_PROD",
+					[]cfclient.GitopsEnvironmentCluster{{
+						Name:        "in-cluster2",
+						RuntimeName: "test-runtime",
+						Namespaces:  []string{"test-ns-1", "test-ns2"},
+					}},
+					[]string{"codefresh.io/environment=test-for-tf", "codefresh.io/environment-1=test-for-tf1"},
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCodefreshGitopsEnvironmentExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", "test-for-tf"),
+					resource.TestCheckResourceAttr(resourceName, "kind", "NON_PROD"),
+					resource.TestCheckResourceAttr(resourceName, "cluster.0.name", "in-cluster2"),
+					resource.TestCheckResourceAttr(resourceName, "cluster.0.runtime_name", "test-runtime"),
+					resource.TestCheckResourceAttr(resourceName, "cluster.0.namespaces.0", "test-ns-1"),
+					resource.TestCheckResourceAttr(resourceName, "cluster.0.namespaces.1", "test-ns2"),
+					resource.TestCheckResourceAttr(resourceName, "label_pairs.0", "codefresh.io/environment=test-for-tf"),
+					resource.TestCheckResourceAttr(resourceName, "label_pairs.1", "codefresh.io/environment-1=test-for-tf1"),
+				),
+			},
+			{
+				Config: testAccCodefreshGitopsEnvironmentConfig(
+					"test-for-tf",
+					"NON_PROD",
+					[]cfclient.GitopsEnvironmentCluster{
+						{
+							Name:        "in-cluster2",
+							RuntimeName: "test-runtime",
+							Namespaces:  []string{"test-ns-1", "test-ns2"},
+						},
+						{
+							Name:        "in-cluster3",
+							RuntimeName: "test-runtime-2",
+							Namespaces:  []string{"test-ns-3"},
+						},
+					},
+					[]string{"codefresh.io/environment=test-for-tf", "codefresh.io/environment-1=test-for-tf1"},
+				),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCodefreshGitopsEnvironmentExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", "test-for-tf"),
+					resource.TestCheckResourceAttr(resourceName, "kind", "NON_PROD"),
+					resource.TestCheckResourceAttr(resourceName, "cluster.0.name", "in-cluster2"),
+					resource.TestCheckResourceAttr(resourceName, "cluster.1.name", "in-cluster3"),
+					resource.TestCheckResourceAttr(resourceName, "cluster.1.runtime_name", "test-runtime-2"),
+					resource.TestCheckResourceAttr(resourceName, "cluster.1.namespaces.0", "test-ns-3"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckCodefreshGitopsEnvironmentExists(resource string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[resource]
+		if !ok {
+			return fmt.Errorf("Not found: %s", resource)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
+
+		envID := rs.Primary.ID
+		apiClient := testAccProvider.Meta().(*cfclient.Client)
+		_, err := apiClient.GetGitopsEnvironmentById(envID)
+		if err != nil {
+			return fmt.Errorf("error fetching gitops environment with ID %s. %s", envID, err)
+		}
+		return nil
+	}
+}
+
+func testAccCheckCodefreshGitopsEnvironmentDestroy(state *terraform.State) error {
+	// Implement destroy check if needed
+	return nil
+}
+
+// CONFIG
+func testAccCodefreshGitopsEnvironmentConfig(name, kind string, clusters []cfclient.GitopsEnvironmentCluster, labelPairs []string) string {
+	var clusterBlocks []string
+	for _, c := range clusters {
+		ns := fmt.Sprintf("[\"%s\"]", strings.Join(c.Namespaces, "\", \""))
+		block := fmt.Sprintf(`  cluster {
+    name        = "%s"
+    runtime_name = "%s"
+    namespaces  = %s
+  }`, c.Name, c.RuntimeName, ns)
+		clusterBlocks = append(clusterBlocks, block)
+	}
+	labelsStr := fmt.Sprintf("[\"%s\"]", strings.Join(labelPairs, "\", \""))
+	return fmt.Sprintf(`
+resource "codefresh_gitops_environment" "test" {
+  name        = "%s"
+  kind        = "%s"
+%s
+  label_pairs = %s
+}
+`, name, kind, strings.Join(clusterBlocks, "\n"), labelsStr)
+}
